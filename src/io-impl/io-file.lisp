86fb1a5aa9e7f40817a4eeec37590e819c5fcd9f
Jason Walker <JasonW94@gmail.com>
2025-12-10 09:06:43 -0500

    finished io-impl

--- src/io-impl/io-file.lisp
+++ src/io-impl/io-file.lisp
@@ -1,10 +1,49 @@
 (cl:in-package :cl-user)
-(in-package :io/file)
+(defpackage :io/io-impl/file
+  (:use
+   #:coalton
+   #:coalton-prelude
+   #:coalton-library/monad/classes
+   #:io/classes/monad-io
+   #:io/classes/monad-io-file
+   #:io/gen-impl/file
+   #:io/io-impl/simple-io
+   )
+  (:local-nicknames
+   (:file #:coalton-library/file))
+  (:export
+   #:with-open-file_
+   #:do-with-open-file_
+   #:with-temp-file_
+   #:do-with-temp-file_
+   #:with-temp-directory_
+   #:do-with-temp-directory_
+   ))
+(in-package :io/io-impl/file)
 
 (named-readtables:in-readtable coalton:coalton)
 
+(coalton-toplevel
+
+  (declare with-open-file_ ((file:File :a) (UnliftIo :m IO) (LiftTo IO :m)
+                            => file:StreamOptions
+                            -> ((file:FileStream :a) -> IO :b)
+                            -> :m :b))
+  (define with-open-file_ with-open-file)
+
+  (declare with-temp-file_ ((file:File :a) (UnliftIo :m IO) (LiftTo IO :m)
+                            => String
+                            -> ((file:FileStream :a) -> IO :b)
+                            -> :m :b))
+  (define with-temp-file_ with-temp-file)
+
+  (declare with-temp-directory_ ((UnliftIo :m IO) (LiftTo IO :m)
+                                 => (file:Pathname -> IO :a)
+                                 -> :m :a))
+  (define with-temp-directory_ with-temp-directory))
+
 (cl:defmacro do-with-open-file_ (opts (fs) cl:&body body)
-  "`do` sugar for `with-open-file_`. Expands to a continuation where BODY runs in `do`."
+  "`do` sugar for `with-open-file_`. Expands to a continuation where BODY runs in `do`.
 
 Usage:
   (do-with-open-file_ opts (fs)
@@ -20,22 +59,3 @@ Usage:
 (cl:defmacro do-with-temp-directory_ ((dir) cl:&body body)
   "`do` sugar for `with-temp-directory_`."
   `(with-temp-directory_ (fn (,dir) (do ,@body))))
-
-(coalton-toplevel
-
-  (declare with-open-file_ ((file:File :a) (UnliftIo :m io:IO) (LiftTo io:IO :m)
-                            => file:StreamOptions
-                            -> ((file:FileStream :a) -> io:IO :b)
-                            -> :m :b))
-  (define with-open-file_ with-open-file)
-
-  (declare with-temp-file_ ((file:File :a) (UnliftIo :m io:IO) (LiftTo io:IO :m)
-                            => String
-                            -> ((file:FileStream :a) -> io:IO :b)
-                            -> :m :b))
-  (define with-temp-file_ with-temp-file)
-
-  (declare with-temp-directory_ ((UnliftIo :m io:IO) (LiftTo io:IO :m)
-                                 => (file:Pathname -> io:IO :a)
-                                 -> :m :a))
-  (define with-temp-directory_ with-temp-directory))
