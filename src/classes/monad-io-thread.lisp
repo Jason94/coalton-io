d28a50921778bcbb50c9a6b7f9394cd0f938e489
Jason Walker <JasonW94@gmail.com>
2025-12-10 23:45:08 -0500

    Started on concurrent+runtime abstractions

--- src/classes/monad-io-thread.lisp
+++ src/classes/monad-io-thread.lisp
@@ -4,6 +4,7 @@
    #:coalton
    #:coalton-prelude
    #:coalton-library/monad/classes
+   #:coalton-library/types
    #:io/classes/monad-io
    #:io/thread-impl/runtime)
   (:import-from #:coalton-library/experimental/do-control-loops-adv
@@ -33,12 +34,52 @@
 
 (coalton-toplevel
 
+  (define-class (Runtime :r :t (:r -> :t))
+    "This class doesn't represent data, but the type tells a Concurrent and
+a MonadIoThread how to hook into the native threading implementations that
+a runtime provides.  A runtime has a 'base' concurrent, which is the underlying
+thread/fiber/etc. that the runtime produces to run concurrently. All other
+Concurrents are built by composing on the base concurrent somehow.
+
+Runtime is a low-level type that operates inside the normal MonadIo layer.
+It should not be used by normal application code. Its two main purposes are:
+(1) to make MonadIoThread generic over the type of thread it forks, and
+(2) to build low-level, efficient concurrency tools that are generic
+over the underlying thread type."
+    (fork!
+     "Spawn a new thread, which starts running immediately.
+Returns the handle to the thread. This version can accept
+any underlying BaseIo, which can be useful, but causes inference
+issues in some cases."
+     (Proxy :r -> :t)))
+
+  ;; TODO: Decide if this should have mask/unmask or not. See below.
+  ;; For now, docstrings are written assuming we'll make masking available.
+  (define-class (Concurrent :c :a (:c -> :a))
+    "A Concurrent is a type that has thread-like semantics. It can be
+stopped, masked, unmasked, and await-ed. Concurrents don't have a uniform
+fork function, becasue they might require different initialization input.
+The most important property of Concurrents is that they can be composed."
+    )
+;;     (stop__
+;;      "Stop a Concurrent. If the thread has already stopped, does nothing.
+;; If the Concurrent is masked, this will pend a stop on the Concurrent. When/if
+;; the Concurrent becomes completely unmaksed, it will stop iself. Regardless
+;; of whether the target Concurrent is masked, STOP does not block or wait for
+;; the target thread to complete."
+     ;; ((MonadIoThread :m :rt) => :c -> :m Unit)))
+
   ;; TODO: To solve all the problems where you can never guarantee re-masking
   ;; in time after handling a thread interrupt, we should actually *re-mask*
   ;; after getting interrupted! Then, any code-path that wants to handle
   ;; an interrupt will need to be aware of that and is responsible for
   ;; un-masking the re-mask, if it wants the thread to keep going.
-  (define-class (MonadIo :m => MonadIoThread :m :t (:m -> :t))
+  ;; ACTUALLY, wait. That might be required for some Concurrent's to work
+  ;; properly. Like a thread group, for example. What we could do is pack some
+  ;; more data into the IoThread word. We could set aside some bits for pending
+  ;; unmasks from other threads, and then treat those unmasks sort of like we
+  ;; treat pending kills. That could help solve some of the race conditions.
+  (define-class (MonadIo :m => MonadIoThread :m :rt (:m -> :rt))
     "A MonadIo which can spawn :t's. Other :t's error
 separately. A spawned :t erroring will not cause the parent
 :t to fail. :t can be any 'thread-like' object, depending on the
@@ -52,13 +93,13 @@ threads, etc."
 Returns the handle to the thread. This version can accept
 any underlying BaseIo, which can be useful, but causes inference
 issues in some cases."
-     ((UnliftIo :r :i) (LiftTo :r :m) => :r :a -> :m :t))
+     ((UnliftIo :r :i) (LiftTo :r :m) (Runtime :rt :t) => :r :a -> :m :t))
     (sleep
      "Sleep the current thread for MSECS milliseconds."
      (UFix -> :m Unit))
     (mask
      "Mask the given thread so it can't be stopped."
-     (:t -> :m Unit))
+     (Concurrent :c :a => :c -> :m Unit))
     (mask-current
      "Mask the current thread so it can't be stopped."
      (:m Unit))
@@ -87,7 +128,7 @@ stopped after being unmasked N times."
      ((UnliftIo :r :io) (LiftTo :r :m) => (UnmaskFinallyMode -> :r Unit) -> :m Unit))
     (stop
      "Stop a thread. If the thread has already stopped, does nothing."
-     (:t -> :m Unit))))
+     (Concurrent :c :a => :c -> :m Unit))))
 
 (cl:defmacro derive-monad-io-thread (monad-param monadT-form)
   "Automatically derive an instance of MonadIoThread for a monad transformer.
