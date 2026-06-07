fefe7c1d5f26263c2fa7426fa7e5cb290249df0d
Jason Walker <JasonW94@gmail.com>
2026-06-07 09:43:54 -0400

    Update docstrings

--- benchmarks/stm.lisp
+++ benchmarks/stm.lisp
@@ -13,6 +13,7 @@
    #:io/monad-io
    #:io/exceptions
    #:io/simple-io
+   #:io/simple-io/loops
    #:io/thread
    #:io/conc/ring-buffer
    #:io/conc/worker-pool
@@ -52,9 +53,9 @@ data structure, such as lock or atomic based ones)."
       (pool <- (new-worker-pool n-threads scheduler))
       ;; Run the benchmark
       (wrap-io (b:start (b:current-timer)))
-      (do-times-io_ n-threads
+      (do-repeat-io n-threads
         (do-submit-job_ pool
-          (do-times-io_ work-per-thread
+          (do-repeat-io work-per-thread
             (do-run-tx
               (modify-tvar tvar 1+)))))
       ;; Cleanup and verify correctness
@@ -90,9 +91,9 @@ synchronized data structure, such as lock or atomic based ones)."
       (pool <- (new-worker-pool n-threads scheduler))
       ;; Run the benchmark
       (wrap-io (b:start (b:current-timer)))
-      (do-times-io_ n-threads
+      (do-repeat-io n-threads
         (do-submit-job_ pool
-          (do-times-io_ iterations-per-thread
+          (do-repeat-io iterations-per-thread
             (do-run-tx
               (modify-tvar tvar1 1+)
               (modify-tvar tvar2 1+)
@@ -182,7 +183,7 @@ case scenario for the STM."
       (wrap-io (b:start (b:current-timer)))
       (do-loop-times (i n-threads)
         (do-submit-job_ pool
-          (do-times-io_ work-per-thread
+          (do-repeat-io work-per-thread
             (do-run-tx
               (x <- (tr:aref# tarr i))
               (tr:set tarr i (1+ x))))))
--- src/io-impl/loops.lisp
+++ src/io-impl/loops.lisp
@@ -33,9 +33,6 @@
   (declare map-into-io ((LiftIo IO :m) (it:IntoIterator :i :a)
                          => :i * (:a -> IO :b) -> :m (List :b)))
   (define (map-into-io itr a->mb)
-    "Efficiently perform a monadic operation for each element of an iterator
-and return the results. More efficient than map-into-io, if you can run your
-effect in a BaseIo."
     (lift-io
      (the
       (IO (List :a))
@@ -49,9 +46,6 @@ effect in a BaseIo."
   (declare foreach-io ((LiftIo IO :m) (it:IntoIterator :i :a)
                         => :i * (c:Cell :a -> IO :b) -> :m Unit))
   (define (foreach-io coll a->mb)
-    "Efficiently perform a monadic operation for each element of an iterator.
-More efficient than foreach-io, if your effect can run in IO. The next element of the
-iterator is passed into the operation via a cell."
     (lift-io
      (the
       (IO Unit)
@@ -73,7 +67,6 @@ iterator is passed into the operation via a cell."
   (inline)
   (declare times-io (LiftIo IO :m => UFix * (c:Cell UFix -> IO :a) -> :m Unit))
   (define (times-io n x->io-op)
-    "Efficiently perform an IO operation N times."
     (lift-io
      (the
       (IO Unit)
@@ -88,7 +81,6 @@ iterator is passed into the operation via a cell."
   (inline)
   (declare repeat-io (LiftIo IO :m => UFix * IO :a -> :m Unit))
   (define (repeat-io n io-op)
-    "Efficiently perform an IO operation N times."
     (lift-io
      (the
       (IO Unit)
@@ -99,15 +91,15 @@ iterator is passed into the operation via a cell."
   )
 
 (defmacro do-map-into-io ((var lst) cl:&body body)
+  "Efficiently perform an IO operation for each element of an iterator and return the
+results."
   `(map-into-io ,lst
      (fn (,var)
        (do
         ,@body))))
 
 (defmacro do-foreach-io ((var-sym into-itr) cl:&body body)
-  "Efficiently perform a monadic operation for each element of an iterator.
-More efficient than foreach-io, if your effect can run in IO. VAR-SYM is bound
-to the value of the element in the iterator."
+  "Efficiently perform an IO operation for each element of an iterator."
   (cl:let ((cell-sym (cl:gensym "iteration-val")))
     `(foreach-io ,into-itr
       (fn (,cell-sym)
@@ -116,7 +108,7 @@ to the value of the element in the iterator."
          ,@body)))))
 
 (defmacro do-times-io ((count-sym n) cl:&body body)
-  "Efficiently perform an IO operation N times."
+  "Efficiently perform an IO operation N times with a counter."
   (cl:let ((cell-sym (cl:gensym "iteration-count")))
     `(times-io ,n
       (fn (,cell-sym)
